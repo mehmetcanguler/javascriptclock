feaa467f45dcc7bba7d95715d0914c3661308d22
Mehmet Can GÜLER <91988777+mehmetcanguler@users.noreply.github.com>
2022-02-27 01:46:05 +0300

    son

--- js/clock.js
+++ js/clock.js
@@ -1,2 +1,8 @@
 let isim=prompt("isminizi giriniz",)
 isimGir=document.querySelector("#myName").innerHTML=isim
+
+function tarihSaat() {
+    var date = new Date().toLocaleString();
+    document.getElementById("myClock").innerHTML = date;
+}
+setInterval(tarihSaat, 1000); 
